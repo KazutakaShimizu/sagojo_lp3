badec45d51f143253acf0329f129c423f50e8c0f
Kazutaka Shimizu <s.kazutaka55555@gmail.com>
2015-11-01 22:15:43 +0900

    ほんとに完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,10 +27,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        UserMailer.welcome_email(@user).deliver_later
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
+        #UserMailer.welcome_email(@user).deliver_later
+        format.html { redirect_to complete_path, notice: 'User was successfully created.' }
+        format.json { render :template => "complete/complete", status: :created, location: @user }
+      else render 
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
